04274892a326521bee7a915031280db8f64e28da
fmoorenoo <fermontemore@gmail.com>
2024-03-05 12:17:04 +0000

    acabado ejer3.py

---
+++ ejer3.py
@@ -0,0 +1,21 @@
+def myDiv(id:str, *clas:str, content:str)->str:
+    resultado = " "
+    resultado += '<div id="' + id + '" '
+
+    if len(clas) != 0:
+        resultado += 'class="'
+
+        for x in clas:
+            resultado += x + ', '
+
+        resultado += '\b\b"'
+    
+    resultado += '>' + content + '</div>'
+    
+    return resultado
+
+def main():
+    print(myDiv("midiv", "bt_h1", "row", "clear", content="Lorem ipsum"))
+
+if __name__ == '__main__':
+    main()
