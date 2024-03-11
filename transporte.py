c82b69274363559a6a6833f3156d33ee828b1dcd
fmoorenoo <fermontemore@gmail.com>
2024-03-11 12:55:29 +0000

    Actualización transporte.py

--- transporte.py
+++ transporte.py
@@ -1,10 +1,10 @@
 class Transporte:
-    def __init__(self, ruedas: int, asientos: int):
+    def __init__(self, ruedas: int, asientos: int) -> None:
         self.ruedas = ruedas
         self.asientos = asientos
 
-    def desplazar(self, x: int, y: int):
-        pass
+    def desplazar(self, x: int, y: int) -> None:
+        print(f"Desplazando a x= {x}; y= {y}.")
 
     def informacion(self):
-        pass
+        print(f"Tiene {self.ruedas} ruedas y {self.asientos} asientos.")
