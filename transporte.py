a2be05bd4d87dc149b77f3dd862cb2cfb2a37042
fmoorenoo <fermontemore@gmail.com>
2024-03-11 12:38:34 +0000

    creado fichero transporte.py

---
+++ transporte.py
@@ -0,0 +1,10 @@
+class Transporte:
+    def __init__(self, ruedas: int, asientos: int):
+        self.ruedas = ruedas
+        self.asientos = asientos
+
+    def desplazar(self, x: int, y: int):
+        pass
+
+    def informacion(self):
+        pass
